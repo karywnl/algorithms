64afc5625b7aabc7868ae42084f99c1c3bfafa75
kary <karywnl@gmail.com>
2026-08-28 08:20:41 +0530

    bst

--- trees/test.py
+++ trees/test.py
@@ -31,6 +31,7 @@ class LinkedBST:
             root.right = self.delete(root.right, value)
 
         else:
+
             if root.left is None:
                 return root.right
 
@@ -45,73 +46,24 @@ class LinkedBST:
             root.value = successor.value
             root.right = self.delete(root.right, successor.value)
 
+
         return root
-        
 
     def inorder(self):
         self._inorder(self.root)
-        print()
 
-    def _inorder(self, node):
-        if node is None:
-            return 
+    def _inorder(self, root):
+        if root is None:
+            return None
 
-        self._inorder(node.left)
-        print(node.value, end=" ")
-        self._inorder(node.right) 
-        
-    
-            
-
-class ArrayBinarySearchTree:
-    def __init__(self, size=15):
-        self.tree = [None] * size
-
-    def insert(self, value):
-        idx = 0
-
-        while True:
-            if idx >= len(self.tree):
-                self.tree.extend(
-                    [None] * (idx - len(self.tree) + 1)
-                )
-
-            if self.tree[idx] is None:
-                self.tree[idx] = value
-                return True
-
-            if value < self.tree[idx]:
-                idx = 2 * idx + 1
-            elif value > self.tree[idx]:
-                idx = 2 * idx + 2
-            else:
-                return False
-
-    def delete(self, value):
-        if value not in self.tree:
-            return False
-
-        idx = self.tree.index(value)
-        remaining = [
-            item
-            for position, item in enumerate(self.tree)
-            if item is not None and position != idx
-        ]
-
-        self.tree = [None] * len(self.tree)
-
-        for item in remaining:
-            self.insert(item)
-
-        return True
+        self._inorder(root.left)
+        print(root.value, end=" ")
+        self._inorder(root.right)
 
 
 bst = LinkedBST()
-
 values = [20, 10, 30, 5, 50]
 for x in values:
     bst.root = bst.insert(bst.root, x)
 
 bst.inorder()
-
-
